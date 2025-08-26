5751e98ad24374b1e676aa36f3fd713d92764395
shadrack <shadrackkinsimba@gmail.com>
2025-08-26 18:33:40 +0300

    feat: access token generation

---
+++ accessToken.php
@@ -0,0 +1,18 @@
+<?php
+//YOU MPESA API KEYS
+$consumerKey = "e2Xy9Y9hGmyQRwbArhu5UxaxDPVfWyRMPiHlrOxyJLMIAowY"; //Fill with your app Consumer Key
+$consumerSecret = "6MVtAnnqccBOQouHS99ZLpGbZ0DOIEy4HYq8h7Ek4YGvoRdjMW291S7Ocf9WAlqP"; //Fill with your app Consumer Secret
+//ACCESS TOKEN URL
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$headers = ['Content-Type:application/json; charset=utf8'];
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+print_r($result);
+// echo $access_token = $result->access_token;
+curl_close($curl);
